9d03ba001d94fd5456cafe211f57e5a57483992e
Diego Ramirez <diegosramirez@gmail.com>
2020-09-24 15:50:55 -0300

    Fixing lambda expression in UseEndpoints

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,7 +32,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
-            app.UseEndpoints(endpoint => endpoint.MapDefaultControllerRoute());
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
